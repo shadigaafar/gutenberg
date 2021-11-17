30210e1227d5d24b83705bed28ca5580c8ae6c24
Aaron Robertshaw <60436221+aaronrobertshaw@users.noreply.github.com>
2021-11-17 14:50:09 +1000

    Extract color and gradient settings retrieval to custom hook

---
+++ packages/block-editor/src/components/colors-gradients/use-common-single-multiple-selects.js
@@ -0,0 +1,12 @@
+/**
+ * Internal dependencies
+ */
+import useSetting from '../use-setting';
+
+export default 
+function useCommonSingleMultipleSelects() {
+	return {
+		disableCustomColors: ! useSetting( 'color.custom' ),
+		disableCustomGradients: ! useSetting( 'color.customGradient' ),
+	};
+}
